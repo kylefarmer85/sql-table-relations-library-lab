8d49ced0b74eea371d3db4481b471d9bde579ea2
kylefarmer85 <kylefarmer85@gmail.com>
2020-09-24 10:17:43 -0500

    unfinished lab

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,11 +12,17 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name 
+  FROM authors 
+  INNER JOIN series ON series.author_id = authors.id 
+  INNER JOIN subgenres ON series.subgenre_id = subgenres.id 
+  GROUP BY authors.id"
 end
 
 def select_series_title_with_most_human_characters
